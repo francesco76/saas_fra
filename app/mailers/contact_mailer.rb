c03aa98778ed6c884d793170b6d5e00143534e27
francesco76 <fr4nkx@gmail.com>
2015-05-24 18:02:49 +0000

    added smtp server to environment.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default to: "fr4nkx@gmail.com"
   
-  def contact_email(name, email)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'mail.zoho.com',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => "francesco.parravicini@netizens.it"#ENV['SENDGRID_USERNAME'],
+  :password => "N3tiz3ns."#ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
+}
