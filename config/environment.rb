f2411adc343cc52848fd50c86d212de06ccda5bc
francesco76 <fr4nkx@gmail.com>
2015-05-24 18:05:37 +0000

    enviromnent.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'mail.zoho.com',
   :port => '587',
   :authentication => :plain,
-  :user_name => "francesco.parravicini@netizens.it"#ENV['SENDGRID_USERNAME'],
-  :password => "N3tiz3ns."#ENV['SENDGRID_PASSWORD'],
+  :user_name => "francesco.parravicini@netizens.it"
+  :password => "N3tiz3ns."
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
