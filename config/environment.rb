af7b06356c3bfa64034039b24c64fce6aecf943e
francesco76 <fr4nkx@gmail.com>
2015-05-25 13:38:14 +0000

    Actionmailer

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.zoho.com',
-  :port => '465',
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
   :authentication => :plain,
-  :user_name => 'francesco.parravicini@netizens.it',
-  :password => 'n3tiz3ns.',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
 }
